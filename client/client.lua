181eabf3fed90f1519254cc50ab71e1a4da53dad
Storm Matteo <86354653+StormDev87@users.noreply.github.com>
2021-07-05 01:21:28 +0200

    minorFix

--- client/client.lua
+++ client/client.lua
@@ -1,15 +1,3 @@
-local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
 ESX             				= nil
 local playerData 				= {}
 local weaponList				= {}
@@ -56,10 +44,8 @@ function OpenMenu()
 				local playerPed  = PlayerPedId()
 				local weaponHash = weapon
 				local pedAmmo = GetAmmoInPedWeapon(playerPed, data.current.arma)
-				--TriggerServerEvent('disassembleWeapon', false, data.current.arma, data.current.armaName, pedAmmo)
 				TriggerServerEvent('disassembleWeapon', false, data.current.arma)
 				Citizen.Wait(800)
-				--OpenMenu()
 			end
 		    end,
 		    function(data, menu)
@@ -83,8 +69,6 @@ function controlWeaponType(weaponHash, group)
 	return result
 end
 
---Chiamato da UseItem 
---passa: numero di munizioni da caricare, massima capacità pistola, nome dell'item da caricare, tipo di item (colpi pistola o smg etc etc), è un box 
 RegisterNetEvent('esx_weapons:useItem')
 AddEventHandler('esx_weapons:useItem', function(data, isComp)
 	local ped = PlayerPedId()
@@ -94,7 +78,7 @@ AddEventHandler('esx_weapons:useItem', function(data, isComp)
 	if IsPedArmed(ped, 4) and (correctType.value or (Config.SingleItem and isComp)) then
 		TriggerServerEvent('esx_weapons:elabData', weaponHash, data, isComp)
 	else
-		ESX.ShowNotification("Non hai l\'arma corretta in mano!", true, true, nil)
+		ESX.ShowNotification(_U('wrong_weapon'), true, true, nil)
 	end
 end)
 
--- locales/it.lua
+++ locales/it.lua
@@ -2,5 +2,6 @@ Locales['it'] = {
 	["disassemble_weapons"] 	= "Smonta Armi",
 	["disassemble"] 			= "Smonta ",
 	["have_weapon"] 			= "Hai già quest'arma",
-	["mounted"] 				= "Hai equipaggiato "
+	["mounted"] 				= "Hai equipaggiato ",
+	["wrong_weapon"]			= "Non hai l\'arma corretta in mano!"
 }
