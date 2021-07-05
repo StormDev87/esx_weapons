6c2f382f196a2a2a942a54ebc582deddc0b4dc97
Storm Matteo <86354653+StormDev87@users.noreply.github.com>
2021-07-05 02:01:51 +0200

    Update esx_weapons.sql

--- esx_weapons.sql
+++ esx_weapons.sql
@@ -1,2 +1,38 @@
+INSERT INTO `items` (`name`, `label`, `desc`, `rare`, `can_remove`, `weight`) VALUES
 
+	('boxpistol', 'Ammo Box Pistola', '', 0, 1, 0.9),
+	('boxrifle', 'Ammo Box Carabine', '', 0, 1, 0.9),
+	('boxshot', 'Ammo Box Fucili', '', 0, 1, 0.9),
+	('boxsmg', 'Ammo Box SMG', '', 0, 1, 0.9),
+	('mpistol', 'Munizione Pistola', '', 0, 1, 0.05),
+	('mrifle', 'Munizione Carabine', '', 0, 1, 0.08),
+	('mshot', 'Munizione Fucili', '', 0, 1, 0.07),
+	('msmg', 'Munizione SMG', '', 0, 1, 0.06),
+	('msniper', 'Munizione Precisione', '', 0, 1, 0.09),
+	('assaultrifle', 'Carabina Assalto', '', 0, 1, 1.3),
+	('assaultshotgun', 'Fucile Assalto', '', 0, 1, 1.6),
+	('assaultsmg', 'SMG Assalto', '', 0, 1, 1.2),
+	('boxsniper', 'Ammo Box Precisione', '', 0, 1, 0.9),
+	('carbinerifle', 'Carabina', '', 0, 1, 1.3),
+	('combatpistol', 'Pistola da Combattimento', '', 0, 1, 0.8),
+	('combatshotgun', 'Fucile da Combattimento', '', 0, 1, 1.3),
+	('compactrifle', 'Carabina Compatta', '', 0, 1, 1.2),
+	('microsmg', 'Micro SMG', '', 0, 1, 0.9),
+	('minismg', 'SMG', '', 0, 1, 0.93),
+	('petrolcan', 'Tanica di benzina', '', 0, 1, 1),
+	('pistol', 'Pistola 9mm', '', 0, 1, 0.75),
+	('pistol50', 'Pistola 50mm', '', 0, 1, 0.85),
+	('pistol_flashlight', 'Flashlight Pistola', '', 0, 1, 1),
+	('pistol_suppressor', 'Soppressore pistola', '', 0, 1, 1),
+	('pumpshotgun', 'Fucile a Pompa', '', 0, 1, 1.2),
+	('smg', 'SMG', '', 0, 1, 0.95),
+	('smokegrenade', 'Granata a Gas', '', 0, 1, 0.5),
+	('sniperifle', 'Fucile Precisione', '', 0, 1, 1.8),
+	('snspistol', 'Pistola SNS', '', 0, 1, 0.85),
+	('specialcarbine', 'Carabina Speciale', '', 0, 1, 1.4),
+	('stungun', 'Tazer', '', 0, 1, 0.8),
+	('generic_flashlight', 'Torcia arma', '', 0, 1, 0.8),
+	
+	
+	
 
